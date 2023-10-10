676a92bda8862751fef2924ca08792beb102b669
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-10-10 16:51:58 -0700

    Add marking system + make board var private

--- javascript.js
+++ javascript.js
@@ -1,27 +1,76 @@
-const gameBoard = (function() {
+const game = (function() {
     const board = [
-        "X", "X", "X", 
-        "O", "X", "X", 
-        "X", "X", "X"
+        "","","",
+        "","","",
+        "","",""
+    ];
+    
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ];
 
     return {
-        render: function() {
+        createBoard: function() {
+            const documentMain = document.querySelector("main");
+            const gameGrid = document.createElement("div");
+            gameGrid.classList.add("gameboard-grid");
+            documentMain.appendChild(gameGrid);
+
+            for (let i = 0; i < 9; i++) {
+                let cell = document.createElement("button");
+                cell.classList.add("cell");
+                cell.setAttribute("data-cellnum", i)
+                gameGrid.appendChild(cell);
+            }
+        },
+        getBoard: function() {
+            return board;
+        }
+    };
+})();
+
+const displayController = (function() {
+    return {
+        renderBoard: function() {
             const cells = document.querySelectorAll(".cell");
     
-            cells.forEach((cell, symbol) => {
-                cell.textContent = board[symbol];
+            cells.forEach((cell, marker) => {
+                cell.textContent = game.getBoard()[marker];
+            });
+        }
+    };
+})();
+
+const misc = (function() {
+    return {
+        clickHandler: function() {
+            window.addEventListener("click", function(event) {
+                if (event.target.className === "play-button") {
+                    event.target.parentNode.replaceChildren();
+                    game.createBoard();
+                }
+                if (event.target.className === "cell") {
+                    let cellNumber = event.target.dataset.cellnum;
+                    game.getBoard().splice(cellNumber, 1, "X");
+                    displayController.renderBoard();
+                }
             });
         }
     }
 })();
 
-gameBoard.render();
+const createPlayer = (name, marker) => {
 
-const displayController = (function() {
-
-})();
-
-const Player = (name) => {
-
+    return { name, marker };
 };
+
+misc.clickHandler();
+
+
